ad41ab98303751d00d9c63b758f3d2121a546fd3
Ajay Sahani <sajay3159@gmail.com>
2024-11-18 12:25:14 +0530

    work on login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 import PlaceOrder from "./pages/placeOrder/PlaceOrder";
 import Footer from "./components/footer/Footer";
+import LoginPopup from "./components/login-popup/LoginPopup";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+      {showLogin ? (
+        <>
+          <LoginPopup setShowLogin={setShowLogin} />
+        </>
+      ) : (
+        <></>
+      )}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
