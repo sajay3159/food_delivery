421f1395e3ff556a757ebf0b998c56ebe28ce787
Ajay Sahani <sajay3159@gmail.com>
2024-11-18 16:37:04 +0530

    Implement Cart UI page layout and design

--- src/components/food-display/FoodDisplay.jsx
+++ src/components/food-display/FoodDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../food-item/FoodItem";
